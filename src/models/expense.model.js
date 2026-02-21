4c44914f3a604054f46d017948322ee08f3598b9
Swaminathan PL <srinath0209@gmail.com>
2026-02-21 22:53:33 +0530

    made the expense model

---
+++ src/models/expense.model.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/db");
+
+const Expense = sequelize.define(
+  "Expense",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name : {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    total_amount : {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+    },
+    currency: {
+      type: DataTypes.STRING(10),
+      allowNull: false,
+      defaultValue: "INR",
+    },
+    date : {
+        type: DataTypes.DATE,
+        allowNull: false
+    }, 
+    createdBy: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    }
+  },
+  {
+    tableName: "expenses",
+    timestamps: true,
+  }
+);
+
+module.exports = Expense;
